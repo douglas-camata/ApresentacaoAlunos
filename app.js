15d4853385f42530ba9b9a7d4ded1adca4164c2a
Douglas Camata de Araujo <douglas.camata@gmail.com>
2025-01-20 18:41:29 -0300

    Ajustes para Vercel

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const path = require('path')
 const bodyParser = require('body-parser');
+
 const app = express();
 const PORT = 3000;
 
 let respostas = [];
 let ipAddresses = new Set();
 
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
